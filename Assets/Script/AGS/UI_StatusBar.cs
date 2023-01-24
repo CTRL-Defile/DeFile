8b0db5cef3cd40dce8e6e2e1f742eaf3c07992ee
GvvangSeop <qufwkfl1149@gmail.com>
2023-01-24 14:23:38 +0900

    Feat : 바코드형식 HP Bar 추가 & HP Bar 적용을 위한 프리팹 수정

--- Assets/Script/AGS/UI_StatusBar.cs
+++ Assets/Script/AGS/UI_StatusBar.cs
@@ -31,13 +31,36 @@ public class UI_StatusBar : MonoBehaviour
     [SerializeField]
     public Character Target_Obj;
     public Slider HPBarSlider;
+	public Slider EffectHPBarSlider;
+
+	[SerializeField]
+    GameObject LineParent;
+
+    [SerializeField]
+    bool m_bEffect = false;
+
+    Canvas canvas;
 
 	// Start is called before the first frame update
 	void Start()
     {
 		Trans = GetComponent<RectTransform>();
         Target_Obj = gameObject.GetComponentInParent<Character>();
-        HPBarSlider = GetComponentInChildren<Slider>();
+
+        if(Bar == STATUS_BAR.HPBar)
+        {
+			EffectHPBarSlider = gameObject.transform.GetChild(1).GetComponent<Slider>();
+			HPBarSlider = gameObject.transform.GetChild(2).GetComponent<Slider>();
+		}
+        else
+        {
+			HPBarSlider = gameObject.transform.GetChild(0).GetComponent<Slider>();
+		}
+
+        canvas = GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        canvas.worldCamera = Camera.main;
+
 
         //FillImage.color = new Color(250, 62, 62, 255);
         //FillImage.color = new Color(51, 183, 46, 255);
@@ -51,7 +74,19 @@ public class UI_StatusBar : MonoBehaviour
         {
             case STATUS_BAR.HPBar:
                 float CurHp = Target_Obj.Stat_HP / Target_Obj.Stat_MaxHP;
-                HPBarSlider.value = CurHp;
+				HPBarSlider.value = CurHp;
+                            
+                Invoke("HpEffectFunc", 1.0f);
+                if(true == m_bEffect)
+                {
+					EffectHPBarSlider.value = Mathf.Lerp(EffectHPBarSlider.value, CurHp, Time.deltaTime * 1.5f);
+
+                    if (HPBarSlider.value >= EffectHPBarSlider.value - 0.01f)
+                    {
+						m_bEffect = false;
+                        EffectHPBarSlider.value = HPBarSlider.value;
+					}                        
+				}				    
                 break;
             case STATUS_BAR.MPBar:
                 float CurMp = Target_Obj.Stat_MP / Target_Obj.Stat_MaxHP;
@@ -65,7 +100,21 @@ public class UI_StatusBar : MonoBehaviour
 
 	private void LateUpdate()
 	{
-		Trans.rotation = Camera.main.transform.rotation;
+		Transform Rootparent = transform.parent;
+
+		if (Bar == STATUS_BAR.HPBar)
+        {           
+            transform.position = Rootparent.position + Vector3.up * (Rootparent.GetComponent<CapsuleCollider>().bounds.size.y + 0.6f);
+			//Trans.position = new Vector3(Target_Obj.transform.position.x, Target_Obj.transform.position.y + 3.1f, Target_Obj.transform.position.z);
+			SetHpLine();
+		}
+            
+        else if(Bar == STATUS_BAR.MPBar)
+        {			
+			transform.position = Rootparent.position + Vector3.up * (Rootparent.GetComponent<CapsuleCollider>().bounds.size.y + 0.4f) + Vector3.forward * -0.1f;
+		}
+	
+		transform.rotation = Camera.main.transform.rotation;
 	}
 
 	public void SetHPColor(STATUS_HP_COLOR color)
@@ -82,4 +131,20 @@ public class UI_StatusBar : MonoBehaviour
                 break;
 		}
     }
+
+    public void SetHpLine()
+    {
+        float scaleX = 100.0f / Target_Obj.Stat_MaxHP;
+		LineParent.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(false);
+        foreach(Transform child in LineParent.transform)
+        {
+            child.gameObject.transform.localScale = new Vector3(scaleX, 1.0f, 1.0f);
+        }
+		LineParent.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(true);
+	}
+
+    void HpEffectFunc()
+    {
+        m_bEffect = true;
+	}
 }
